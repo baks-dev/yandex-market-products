d7d3dd6ee311459214432e288c16664d7337c04f
kingstar <admin@baks.dev>
2024-06-03 22:55:40 +0300

    Baks Development

--- Repository/Card/CurrentYaMarketProductsCard/YaMarketProductsCardRepository.php
+++ Repository/Card/CurrentYaMarketProductsCard/YaMarketProductsCardRepository.php
@@ -601,8 +601,6 @@ final class YaMarketProductsCardRepository implements YaMarketProductsCardInterf
         $dbal->allGroupByExclude();
 
         /** Кешируем в кеш модуля products-product для сброса при обновлении карточки */
-        return $dbal
-            ->enableCache('products-product', 60)
-            ->fetchAssociative();
+        return $dbal->fetchAssociative();
     }
 }

2800cdf4f9ec615155deb78dfdabcebe757aafc1
kingstar <admin@baks.dev>
2024-06-10 10:53:29 +0300

    Baks Development

--- BaksDevYandexMarketProductsBundle.php
+++ BaksDevYandexMarketProductsBundle.php
@@ -35,4 +35,24 @@ class BaksDevYandexMarketProductsBundle extends AbstractBundle
     public const NAMESPACE = __NAMESPACE__.'\\';
 
     public const PATH = __DIR__.DIRECTORY_SEPARATOR;
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $services = $container->services()
+            ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+        $services->load(self::NAMESPACE, self::PATH)
+            ->exclude([
+                self::PATH.'{Entity,Resources,Type}',
+                self::PATH.'**/*Message.php',
+                self::PATH.'**/*DTO.php',
+            ]);
+
+        $services->load(
+            self::NAMESPACE.'Type\Settings\Property\Properties\\',
+            self::PATH.'Type/Settings/Property/Properties'
+        );
+    }
 }

bb3bb8a09fff987d17a360f513e2954e084ad618
kingstar <admin@baks.dev>
2024-06-23 19:29:42 +0300

    Baks Development

---
+++ Type/Settings/Property/Properties/Tire/LifeTimeYaMarketProductProperty.php
@@ -0,0 +1,118 @@
+<?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Yandex\Market\Products\Type\Settings\Property\Properties\Tire;
+
+use BaksDev\Yandex\Market\Products\Repository\Card\CurrentYaMarketProductsCard\YaMarketProductsCardInterface;
+use BaksDev\Yandex\Market\Products\Type\Card\Id\YaMarketProductsCardUid;
+use BaksDev\Yandex\Market\Products\Type\Settings\Property\Properties\Collection\YaMarketProductPropertyInterface;
+use BaksDev\Yandex\Market\Products\Type\Settings\Property\YaMarketProductProperty;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+#[AutoconfigureTag('baks.ya.product.property')]
+final class LifeTimeYaMarketProductProperty implements YaMarketProductPropertyInterface
+{
+    /**
+     * Срок службы — период, в течение которого товар должен исправно выполнять свою функцию.
+     *
+     * Обязательно указывайте срок, если он есть.
+     *
+     * В комментарии укажите условия хранения. Например, «Использовать при температуре не ниже −10 градусов».
+     */
+    public const string PARAM = 'lifeTime';
+
+    public function __construct(
+        private readonly ?YaMarketProductsCardInterface $yaMarketProductsCard = null
+    ) {}
+
+    public function getValue(): string
+    {
+        return self::PARAM;
+    }
+
+    public function default(): ?string
+    {
+        return null;
+    }
+
+    public function required(): bool
+    {
+        return false;
+    }
+
+    public function choices(): ?array
+    {
+        return null;
+    }
+
+    /**
+     * Сортировка (чем меньше число - тем первым в итерации будет значение)
+     */
+    public static function priority(): int
+    {
+        return 500;
+    }
+
+    /**
+     * Проверяет, относится ли статус к данному объекту
+     */
+    public static function equals(string $status): bool
+    {
+        return self::PARAM === $status;
+    }
+
+
+    public function isSetting(): bool
+    {
+        return true;
+    }
+
+    public function isCard(): bool
+    {
+        return true;
+    }
+
+    public function getData(YaMarketProductsCardUid|array $card): mixed
+    {
+
+        if($this->yaMarketProductsCard)
+        {
+            $data = $card instanceof YaMarketProductsCardUid ? $this->yaMarketProductsCard->findByCard($card) : $card;
+
+            if(!isset($data['market_category']) || $data['market_category'] !== YaMarketProductProperty::CATEGORY_TIRE)
+            {
+                return null;
+            }
+
+            return [
+                'timePeriod' => 5,
+                'timeUnit' => 'YEAR',
+            ];
+        }
+
+        return null;
+
+    }
+}

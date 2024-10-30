5a6ccaf30b5a440da563a3f6316260d2d5cd2b4b
kingstar <admin@baks.dev>
2024-10-31 00:35:40 +0300

    Baks Development

--- Api/Products/Stocks/YaMarketProductUpdateStocksRequest.php
+++ Api/Products/Stocks/YaMarketProductUpdateStocksRequest.php
@@ -1,17 +1,17 @@
 <?php
 /*
  *  Copyright 2024.  Baks.dev <admin@baks.dev>
- *
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,6 +31,8 @@ use InvalidArgumentException;
 
 final class YaMarketProductUpdateStocksRequest extends YandexMarket
 {
+    private const bool STOP_SALES = false;
+
     private ?string $article = null;
 
     private int $total = 0;
@@ -80,7 +82,7 @@ final class YaMarketProductUpdateStocksRequest extends YandexMarket
                             [
                                 'sku' => $this->article,
                                 "items" => [
-                                    ["count" => max($this->total, 0)]
+                                    ["count" => self::STOP_SALES === true ? 0 : max($this->total, 0)]
                                 ]
                             ]
                         ]

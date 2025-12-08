86ca14395c01bed3ebf74bf12291c16f9e949eb5
MichelAngelo <baksdevelopmen@gmail.com>
2025-12-09 01:49:50 +0300

    BaksDevelopment

--- UseCase/Cards/NewEdit/YaMarketProductsCardHandler.php
+++ UseCase/Cards/NewEdit/YaMarketProductsCardHandler.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,32 +29,17 @@ namespace BaksDev\Yandex\Market\Products\UseCase\Cards\NewEdit;
 use BaksDev\Core\Entity\AbstractHandler;
 use BaksDev\Yandex\Market\Products\Entity\Card\Event\YaMarketProductsCardEvent;
 use BaksDev\Yandex\Market\Products\Entity\Card\YaMarketProductsCard;
-use BaksDev\Yandex\Market\Products\Messenger\Card\YaMarketProductsCardMessage;
-use DomainException;
 
 final class YaMarketProductsCardHandler extends AbstractHandler
 {
 
     /** @see YaMarketProductsCard */
-    public function handle(
-        YaMarketProductsCardDTO $command
-    ): string|YaMarketProductsCard
+    public function handle(YaMarketProductsCardDTO $command): string|YaMarketProductsCard
     {
 
-        /** Валидация DTO  */
-        $this->validatorCollection->add($command);
-
-        $this->main = new YaMarketProductsCard();
-        $this->event = new YaMarketProductsCardEvent();
-
-        try
-        {
-            $command->getEvent() ? $this->preUpdate($command, true) : $this->prePersist($command);
-        }
-        catch(DomainException $errorUniqid)
-        {
-            return $errorUniqid->getMessage();
-        }
+        $this
+            ->setCommand($command)
+            ->preEventPersistOrUpdate(YaMarketProductsCard::class, YaMarketProductsCardEvent::class);
 
         /** Валидация всех объектов */
         if($this->validatorCollection->isInvalid())
@@ -62,13 +47,13 @@ final class YaMarketProductsCardHandler extends AbstractHandler
             return $this->validatorCollection->getErrorUniqid();
         }
 
-        $this->entityManager->flush();
+        $this->flush();
 
-        /* Отправляем сообщение в шину */
-        $this->messageDispatch->dispatch(
-            message: new YaMarketProductsCardMessage($this->main->getId(), $this->main->getEvent(), $command->getEvent()),
-            transport: (string) $command->getMarket()->getProfile()
-        );
+        //        /* Отправляем сообщение в шину */
+        //        $this->messageDispatch->dispatch(
+        //            message: new YaMarketProductsCardMessage($this->main->getId(), $this->main->getEvent(), $command->getEvent()),
+        //            transport: (string) $command->getMarket()->getProfile(),
+        //        );
 
         return $this->main;
     }
